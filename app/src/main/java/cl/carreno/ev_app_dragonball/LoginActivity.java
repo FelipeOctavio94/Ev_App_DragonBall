672f084e13116d80852c7701f7f49b82c2d735e5
felipe <felipe.carreno@hotmail.com>
2020-10-06 12:48:15 -0300

    Primera parte de API

--- app/src/main/java/cl/carreno/ev_app_dragonball/LoginActivity.java
+++ app/src/main/java/cl/carreno/ev_app_dragonball/LoginActivity.java
@@ -5,14 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
 
-public class loginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
-    EditText txt;
+    private String user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        txt = findViewById(R.id.user)
+
+
+
     }
 }
--- app/src/main/java/cl/carreno/ev_app_dragonball/MainActivity.java
+++ app/src/main/java/cl/carreno/ev_app_dragonball/MainActivity.java
@@ -2,13 +2,69 @@ package cl.carreno.ev_app_dragonball;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText txt;
+    private static final String URL="https://dragon-ball-api.herokuapp.com/api/character/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txt = findViewById(R.id.user);
+    }
+
+    public void Login(View view) {
+        if (txt.getText().toString().isEmpty()){
+            Toast.makeText(this,"Please complete your user",Toast.LENGTH_LONG).show();
+        }else {
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    public void processHttp(){
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(URL, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                String data = new String(responseBody);
+                Log.d("INFO",data);
+                processHttp(data);
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+
+            }
+        });
+    }
+
+    public void processHttp(String data) {
+        try {
+            JSONObject root = new JSONObject(data);
+            JSONArray result = root.getJSONArray("characters");
+        }catch (JSONException e){
+            e.printStackTrace();
+        }
+
+
     }
 }
